1732d941e38ea483ea86dea99537909310d2abaf
Diego Hernandes <diegowalhernandes@gmail.com>
2024-06-13 18:12:17 -0300

    creating login page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container" style="padding-top: 10%>
+      <div class="row">
+        <div class="col-sm-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nemo deserunt laboriosam? Nam ab, repellendus hic omnis iste, quisquam aut corrupti sunt molestiae ea itaque voluptate beatae voluptas deserunt earum.
+        </div>
+        <div class="col-sm-6">
+            <form action="" method="">
+            <div class="mb-3">
+              <label>Email</label>
+              <input type="email" class="form-control" name="email" placeholder="name@example.com" require>
+            </div>
+            <div class="mb-3">
+              <labelSenha</label>
+              <input type="password" class="form-control" name="senha" required>
+            </div>
+            <div style="text-align: right;"> 
+                <button type="submit" class="btn btn-primary">Acessar</button>
+            </div>
+            <br>
+            <div style="text-align: center">
+              <a href="cadastro.php">Cadastre-se</a>
+            </div>
+            </form>
+        </div>
+      </div>
+      <tr></tr>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
